3047102e3aceb8f44c4e8a94dcda841bf4636ce0
LachezarTomov <lbtomov@gmail.com>
2014-11-11 00:55:37 +0200

    Added LikesController.cs and likes functionality

---
+++ StartupJointVEnture.Web/Controllers/LikesController.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace StartupJointVenture.Web.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Microsoft.AspNet.Identity;
+
+    using AutoMapper.QueryableExtensions;
+    using StartupJointVenture.Models;
+
+    public class LikesController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LikeIdea(int ideaId, int likesCount)
+        {
+            if (ModelState.IsValid)
+            {
+                string userId = User.Identity.GetUserId();
+                var like = this.Data.Likes
+                    .All()
+                    .Where(l => l.AuthorId == userId)
+                    .Where(l => l.IdeaId == ideaId)
+                    .FirstOrDefault();
+
+                if (like == null)
+                {
+                    Like newLike = new Like()
+                    {
+                        AuthorId = userId,
+                        IdeaId = ideaId
+                    };
+
+                    this.Data.Likes.Add(newLike);
+                    this.Data.SaveChanges();
+                    likesCount++;
+                }
+            }
+            return Content(likesCount.ToString());
+        }
+    }
+}
